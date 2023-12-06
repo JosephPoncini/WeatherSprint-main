149a4cc6151fd6aa839d91e93e086b1518c0c7a5
JosephPoncini <jxponcini@gmail.com>
2023-12-06 11:52:48 -0800

    all data fetched and calculated

--- scripts/app.js
+++ scripts/app.js
@@ -34,6 +34,8 @@ let currentTemp;
 let todaysHigh;
 let todaysLow;
 let currentTime;
+let currentWeatherMain;
+let currentWeatherDescription;
 let timeZone;
 
 let currentDayOfTheWeek;
@@ -107,9 +109,12 @@ async function GetCurrentWeatherData() {
     currentTime = convertSecondsToHHMM(dt + timeZone);
     todaysHigh = data.main.temp_max;
     todaysLow = data.main.temp_min;
+    currentWeatherMain = data.weather[0].main;
+    currentWeatherDescription = data.weather[0].description;
+
 
     //----------------------------------------------GetDaysofWeek
-    currentDayOfTheWeek = GetCurrentDayOfWeek(dt+timeZone);
+    currentDayOfTheWeek = GetCurrentDayOfWeek(dt + timeZone);
     day1Name = GetDayOfWeek(currentDayOfTheWeek);
     day2Name = GetDayOfWeek(day1Name);
     day3Name = GetDayOfWeek(day2Name);
@@ -119,19 +124,21 @@ async function GetCurrentWeatherData() {
     //----------------------------------------------Console Logging
     console.log("The latitude is " + lat);
     console.log("The longitude is " + lon);
-    console.log("Today is " + currentDayOfTheWeek) 
-    console.log("Day 1 is "+ day1Name);
-    console.log("Day 2 is "+ day2Name);
-    console.log("Day 3 is "+ day3Name);
-    console.log("Day 4 is "+ day4Name);
-    console.log("Day 5 is "+ day5Name);
+    console.log("Today is " + currentDayOfTheWeek)
+    console.log("Day 1 is " + day1Name);
+    console.log("Day 2 is " + day2Name);
+    console.log("Day 3 is " + day3Name);
+    console.log("Day 4 is " + day4Name);
+    console.log("Day 5 is " + day5Name);
 
     console.log("The date is " + date);
     console.log("The time is " + currentTime);
     console.log("Your timezone is: " + timeZone);
     console.log("The current Temperature is " + currentTemp + "C");
     console.log("The max temp today is: " + todaysHigh + "C");
-    console.log("The min temp today is: " + todaysLow + "C")
+    console.log("The min temp today is: " + todaysLow + "C");
+    console.log("The general weather right now is " + currentWeatherMain);
+    console.log("Specifically right now it is " + currentWeatherDescription);
 
 }
 
@@ -149,68 +156,70 @@ async function GetFiveDayData() {
     let midMaxTemps = FillMaxTempArray(data);
     let midMinTemps = FillMinTempArray(data);
     let midTimes = FillTimeArray(data, timeZone)
+    let weathers = FillWeatherArray(data);
+    console.log(weathers);
 
     //------------------- estimated temps calcs start here
     //#region 
-    
-    
-        firstTime = data.list[0].dt + timeZone;
-        lastTime = data.list[39].dt + timeZone;
-        startTime = firstTime - (firstTime % 86400) + (hourOffset * 3600);
-        endTime = startTime + 518400;
 
-        let n = (firstTime - startTime) / 10800; // number of three hour segments from midnight today to first time bracket
-        let earlierTime = [];
-        let earlyZeros = [];
-        for (let i = 0; i < n; i++) {
-            earlierTime[i] = startTime + (i * 10800);
-            earlyZeros[i] = 0;
-        }
 
-        let earlyTemps = [];
-        for (let i = 0; i < n; i++) {
-            let x = [];
-            let y = [];
-            for (let j = 0; j < 5; j++) {
-                x[j] = midTimes[(8 - n) + (j * 8) + i];
-                y[j] = midTemps[(8 - n) + (j * 8) + i];
-            }
-            let regressionLine = linearRegression(x, y);
-            earlyTemps[i] = (regressionLine.slope * earlierTime[i] + regressionLine.intercept).toFixed(2);
-        }
+    firstTime = data.list[0].dt + timeZone;
+    lastTime = data.list[39].dt + timeZone;
+    startTime = firstTime - (firstTime % 86400) + (hourOffset * 3600);
+    endTime = startTime + 518400;
 
-        let m = (endTime - lastTime) / 10800; // number of three hour segments from last given time slot to midnight of the following day
-        let laterTime = [];
-        let lateZeros = [];
-        for (let i = 1; i < m; i++) {
-            laterTime[i - 1] = lastTime + (i * 10800);
-            lateZeros[i - 1] = 0;
-        }
+    let n = (firstTime - startTime) / 10800; // number of three hour segments from midnight today to first time bracket
+    let earlierTime = [];
+    let earlyZeros = [];
+    for (let i = 0; i < n; i++) {
+        earlierTime[i] = startTime + (i * 10800);
+        earlyZeros[i] = 0;
+    }
 
-        let laterMaxTemps = [];
-        for (let i = 1; i < m; i++) {
-            let x = [];
-            let y = [];
-            for (let j = 0; j < 5; j++) {
-                x[j] = midTimes[(8 - m) + (j * 8) + i];
-                y[j] = midMaxTemps[(8 - m) + (j * 8) + i];
-            }
-            let regressionLine = linearRegression(x, y);
-            laterMaxTemps[i - 1] = (regressionLine.slope * laterTime[i - 1] + regressionLine.intercept).toFixed(2);
+    let earlyTemps = [];
+    for (let i = 0; i < n; i++) {
+        let x = [];
+        let y = [];
+        for (let j = 0; j < 5; j++) {
+            x[j] = midTimes[(8 - n) + (j * 8) + i];
+            y[j] = midTemps[(8 - n) + (j * 8) + i];
         }
+        let regressionLine = linearRegression(x, y);
+        earlyTemps[i] =  parseFloat((regressionLine.slope * earlierTime[i] + regressionLine.intercept).toFixed(2));
+    }
 
-        let laterMinTemps = [];
-        for (let i = 1; i < m; i++) {
-            let x = [];
-            let y = [];
-            for (let j = 0; j < 5; j++) {
-                x[j] = midTimes[(8 - m) + (j * 8) + i];
-                y[j] = midMinTemps[(8 - m) + (j * 8) + i];
-            }
-            let regressionLine = linearRegression(x, y);
-            laterMinTemps[i - 1] = (regressionLine.slope * laterTime[i - 1] + regressionLine.intercept).toFixed(2);
+    let m = (endTime - lastTime) / 10800; // number of three hour segments from last given time slot to midnight of the following day
+    let laterTime = [];
+    let lateZeros = [];
+    for (let i = 1; i < m; i++) {
+        laterTime[i - 1] = lastTime + (i * 10800);
+        lateZeros[i - 1] = 0;
+    }
+
+    let laterMaxTemps = [];
+    for (let i = 1; i < m; i++) {
+        let x = [];
+        let y = [];
+        for (let j = 0; j < 5; j++) {
+            x[j] = midTimes[(8 - m) + (j * 8) + i];
+            y[j] = midMaxTemps[(8 - m) + (j * 8) + i];
         }
-    
+        let regressionLine = linearRegression(x, y);
+        laterMaxTemps[i - 1] =  parseFloat((regressionLine.slope * laterTime[i - 1] + regressionLine.intercept).toFixed(2));
+    }
+
+    let laterMinTemps = [];
+    for (let i = 1; i < m; i++) {
+        let x = [];
+        let y = [];
+        for (let j = 0; j < 5; j++) {
+            x[j] = midTimes[(8 - m) + (j * 8) + i];
+            y[j] = midMinTemps[(8 - m) + (j * 8) + i];
+        }
+        let regressionLine = linearRegression(x, y);
+        laterMinTemps[i - 1] = parseFloat((regressionLine.slope * laterTime[i - 1] + regressionLine.intercept).toFixed(2));
+    }
+
 
     //#endregion
     //------------------- estimated temps calcs end here
@@ -234,27 +243,42 @@ async function GetFiveDayData() {
     day4Low = GetDaysMinTemp(minTemps, 4);
     day5Low = GetDaysMinTemp(minTemps, 5);
 
+    console.log(maxTemps);
+    console.log(minTemps);
+
     //---------------------------------------Get 8am 12pm 8pm temps
 
-    switch(hourOffset){
+    switch (hourOffset) {
         case 0:
-            temp8am = ((temps[2] + (2*temps[3]))/3).toFixed(2);
+            temp8am = ((temps[2] + (2 * temps[3])) / 3).toFixed(2);
             temp12pm = temps[4].toFixed(2);
-            temp8pm = ((temps[6] + (2*temps[7]))/3).toFixed(2);
-        break;
+            temp8pm = ((temps[6] + (2 * temps[7])) / 3).toFixed(2);
+            break;
         case 1:
-            temp8am = (((2*temps[2]) + temps[3])/3).toFixed(2);
-            temp12pm = ((temps[3] + (2*temps[4]))/3).toFixed(2);
-            temp8pm = (((2*temps[6]) + temps[7])/3).toFixed(2);
-        break;
+            temp8am = (((2 * temps[2]) + temps[3]) / 3).toFixed(2);
+            temp12pm = ((temps[3] + (2 * temps[4])) / 3).toFixed(2);
+            temp8pm = (((2 * temps[6]) + temps[7]) / 3).toFixed(2);
+            break;
         case 2:
             temp8am = temps[2].toFixed(2);
-            temp12pm = (((2*temps[3]) + temps[4])/3).toFixed(2);
+            temp12pm = (((2 * temps[3]) + temps[4]) / 3).toFixed(2);
             temp8pm = temps[6].toFixed(2);
-        break;
+            break;
         default:
-        break;
+            break;
     }
+
+
+    //------------------------------------- Getting Forecasted Weather
+
+
+
+    day1Weather = DetermineForecastedWeather(weathers, 1, m);
+    day2Weather = DetermineForecastedWeather(weathers, 2, m);
+    day3Weather = DetermineForecastedWeather(weathers, 3, m);
+    day4Weather = DetermineForecastedWeather(weathers, 4, m);
+    day5Weather = DetermineForecastedWeather(weathers, 5, m);
+
 
 
 
@@ -274,6 +298,11 @@ async function GetFiveDayData() {
     console.log("Day 5 Max Temp is " + day5High);
     console.log("Day 5 Min Temp is " + day5Low);
 
+    console.log("On Day 1 it will be " + day1Weather);
+    console.log("On Day 2 it will be " + day2Weather);
+    console.log("On Day 3 it will be " + day3Weather);
+    console.log("On Day 4 it will be " + day4Weather);
+    console.log("On Day 5 it will be " + day5Weather);
     // console.log(maxTemps);
     // console.log(minTemps);
 
@@ -284,10 +313,10 @@ function GetCurrentDayOfWeek(unixTimestamp) {
     const milliseconds = unixTimestamp * 1000;
     const dateObject = new Date(milliseconds);
 
-    return dateObject.toLocaleString('en-US', { weekday: 'long', timeZone: 'UTC'});
+    return dateObject.toLocaleString('en-US', { weekday: 'long', timeZone: 'UTC' });
 }
 
-function GetDayOfWeek(yesterday){
+function GetDayOfWeek(yesterday) {
 
     let today;
 
@@ -295,7 +324,7 @@ function GetDayOfWeek(yesterday){
         case "Sunday":
             today = "Monday";
             break;
-        
+
         case "Monday":
             today = "Tuesday";
             break;
@@ -370,6 +399,16 @@ function FillMinTempArray(data) {
     return tempMinArray;
 }
 
+function FillWeatherArray(data) {
+    let weatherArray = [];
+
+    for (let i = 0; i < 40; i++) {
+        weatherArray[i] = data.list[i].weather[0].main;
+    }
+
+    return weatherArray;
+}
+
 function FillTimeArray(data, timeZone) {
     let timeArray = [];
 
@@ -425,4 +464,37 @@ function linearRegression(x, y) {
     return { slope, intercept };
 }
 
-console.log(convertSecondsToHHMM(1701734400));
+function DetermineForecastedWeather(weathers, dayNumber, m) {
+    weatherOptions = ['Snow', 'Thunderstorm', 'Rain', 'Drizzle', 'Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall', 'Tornado', 'Clouds', 'Clear'];
+
+    weatherTriggers = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+    if (dayNumber == 5) {
+        for (let i = 0; i < (8 - m); i++) {
+            let currentWeather = weathers[8 * (dayNumber - 1) + m];
+            for(let j = 0; j < 15; j++){
+                if(currentWeather == weatherOptions[j]){
+                    weatherTriggers[j] = true;
+                }
+            }
+        }
+    }else{
+        for (let i = 0; i < 8; i++) {
+            let currentWeather = weathers[8 * (dayNumber - 1) + m];
+            for(let j = 0; j < 15; j++){
+                if(currentWeather == weatherOptions[j]){
+                    weatherTriggers[j] = true;
+                }
+            }
+        }
+    }
+
+    for(let i = 0; i < 15; i++){
+        if(weatherTriggers[i]){
+            return weatherOptions[i];
+        }
+    }
+
+    return "Something went wrong";
+
+}
